ac617344d5b83c71e8c10fb67d7a0bb92f6eea63
= <=>
2020-03-18 14:52:27 +0530

    [Vishnu]Refactor.Added proper naming conventions

--- Test/gamblerTest.js
+++ Test/gamblerTest.js
@@ -1,34 +1,34 @@
 var assert = require('chai').assert;
 let callFunction = require('../Gambler/GamblerBL');
 describe("This is test code for gambler program", () => {
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 1", () => {
         assert.equal(callFunction.gambling(150, 250, 50, 3));
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 2", () => {
         assert.equal(callFunction.gambling(1500, 2000, 100, 3));
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 3", () => {
         assert.equal(callFunction.gambling());
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 4", () => {
         assert.false(callFunction.gambling(150, notdefined));
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 5", () => {
         assert.equal(callFunction.gambling(null))
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 6", () => {
         assert.equal(callFunction.gambling(2000, null, not))
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 7", () => {
         assert.equal(callFunction.gambling(2000, 1500, 3000))
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 8", () => {
         assert.equal(callFunction.gambling(2000, 1))
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 9", () => {
         assert.equal(callFunction.gambling(200, 150, 350))
     });
-    it("givenInputs_whenChecked_shouldReturnEqual", () => {
+    it("givenInputs_whenChecked_shouldReturnEqual 10", () => {
         assert.equal(callFunction.gambling(2000, 5000, 1999, 3))
     });
 });
