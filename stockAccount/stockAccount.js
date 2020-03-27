1051db29da47d521286a5c2ba41044bbcec5c286
= <=>
2020-03-27 15:05:31 +0530

    [Vishnu]Refactor.Added function to add stock and print the stock report

--- stockAccount/stockAccount.js
+++ stockAccount/stockAccount.js
@@ -2,11 +2,24 @@ const path = require('./stockAccountBL');
 const fs = require('fs').promises;
 let input = require('readline-sync')
 async () => { //reading json file using async await
-    const stock = await fs.readFile('address.json');
-    this.stockData = JSON.parse(stock);
+    const jsonData = await fs.readFile('address.json');
+    this.stockData = JSON.parse(jsonData);
 }
 try {
-    let stockAcc = path.StockAccount();
+    let stockAcc = new path.StockAccount();
+    var ch = Number(input.questionInt("Choose from the given options \n" +
+        "1 - Add Stock \n2 - print report \n "))
+    switch (ch) {
+        case 1:
+            stockAcc.addStock();
+            break;
+        case 2:
+            stockAcc.printReport();
+        case 3:
+            process.exit(0);
+        default:
+            console.log("invalid");
+    }
 }
 catch (err) {
     console.log(err)
--- stockAccount/stockAccountBL.js
+++ stockAccount/stockAccountBL.js
@@ -7,3 +7,31 @@ class Stock {
         this.price = price;
     }
 }
+class StockAccount {
+    constructor() {
+    }
+    addStock = async () => {
+        try {
+            let name = readLine.question('Enter name of stock : ');
+            let numOfStock = readLine.questionInt('Enter number of stock : ');
+            let price = readLine.questionInt('Enter the price : ');
+            let stock = new Stock(name, numberOfStock, price);
+            this.stockData.stock.push(JSON.parse(JSON.stringify(stock)));
+            await fs.writeFileSync('stockDetails.json', JSON.stringify(this.stockData));
+            console.log(' entry added to the file ')
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+    printReport = async () => {
+        for (let i in this.stockData.stock) {
+            let totalPrice = this.stockData.stock[i].price * this.stockData.stock[i].numOfStock;
+            console.log('Total price = ', totalPrice);
+        }
+
+    }
+}
+module.exports = {
+    StockAccount
+}
