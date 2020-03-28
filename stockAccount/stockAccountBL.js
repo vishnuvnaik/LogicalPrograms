4a305f38a4fb3e50824bd998b65f26694ccb685f
= <=>
2020-03-28 15:00:08 +0530

    [Vishnu]Refactor.added buystock class and function

--- stockAccount/stockAccountBL.js
+++ stockAccount/stockAccountBL.js
@@ -31,6 +31,29 @@ class StockAccount {
         return position;
     }
 }
+class BuyStock extends StockAccount {
+    constructor() {
+        super()
+    }
+    buyStock(custIndex) {
+        console.log(`-------** Welcom ${this.stockData.customer[custIndex].name}..`);
+        let name = input.question('Enter the name of Stock : ');
+        if (!nameRestriction.test(name)) { //input validation of name 
+            this.name = input.question("No special characters Invalid name! :");
+        }
+        let numOfStock = input.questionInt('Enter Number of stock : ');
+        if (!numRestriction.test(numOfStock)) { //input validation of numOfStock
+            this.numOfStock = input.question("Enter digits only : ");
+        }
+        let price = input.questionInt('Enter the price of Stock : ');
+        if (!numRestriction.test(price)) { //input validation of numOfStock
+            this.price = input.question("Enter digits only : ");
+        }
+        let buyStock = new Stock(name, numOfStock, price);
+        this.stockData.customer[custIndex].stock.push(JSON.parse(JSON.stringify(buyStock)));
+        fs.writeFileSync('stock.json', JSON.stringify(this.stockData));
+    }
+}
 
 
 module.exports = {
