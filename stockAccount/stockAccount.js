22e939257b6dd2fa5a8247fafbc6ab1bd75ccb1a
= <=>
2020-03-28 15:02:23 +0530

    [Vishnu]Refactor.added new features to check user input

--- stockAccount/stockAccount.js
+++ stockAccount/stockAccount.js
@@ -7,22 +7,39 @@ async () => { //reading json file using async await
 }
 try {
     let stockAcc = new path.StockAccount();
-    let stockAccPrint = new path.PrintStock();
-    var ch = Number(input.questionInt("Choose from the given options \n" +
-        "1 - Add Stock \n2 - print report \n "))
-    switch (ch) {
-        case 1:
-            stockAcc.addStock();
-            break;
-        case 2:
-            stockAccPrint.printReport();
-            break;
-        case 3:
-            process.exit(0);
-        default:
-            console.log("invalid");
+    let stockPrint = new path.PrintStock();
+    let stockSell = new path.SellStock();
+    let stockBuy = new path.BuyStock();
+    //let stockAccPrint = new path.PrintStock();
+    let userName = input.question('Enter Username = ');
+    let password = input.question('Enter the Password = ');
+    let reslt = stockAcc.checkAccount(userName, password);
+    if (reslt != -1) {
+        console.log('Successfully Logged In');
+        stockPrint.printReport(reslt);
+        var ch = Number(input.questionInt("Choose from the given options \n" +
+            "1 - buy Stock \n2 - Sell stock \n3 - print report \n "))
+        switch (ch) {
+            case 1:
+                stockBuy.buyStock(reslt);
+                break;
+            case 2:
+                stockSell.sellStock(reslt);
+                break;
+            case 3:
+                stockPrint.printReport(reslt);
+                break;
+            case 3:
+                process.exit(0);
+            default:
+                console.log("invalid");
+        }
+    }
+    else {
+        console.log("credentials not found ")
     }
 }
+
 catch (err) {
     console.log(err)
 }
