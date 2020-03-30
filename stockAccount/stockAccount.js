84099cd7f1763fa59e18c7ca03317ce3914c026e
= <=>
2020-03-30 09:54:35 +0530

    [Vishnu]Refactor.modified switch statement and added create acc function

--- stockAccount/stockAccount.js
+++ stockAccount/stockAccount.js
@@ -10,36 +10,43 @@ try {
     let stockPrint = new path.PrintStock();
     let stockSell = new path.SellStock();
     let stockBuy = new path.BuyStock();
-    //let stockAccPrint = new path.PrintStock();
-    let userName = input.question('Enter Username = ');
-    let password = input.question('Enter the Password = ');
-    let reslt = stockAcc.checkAccount(userName, password);
-    if (reslt != -1) {
-        console.log('Successfully Logged In');
-        stockPrint.printReport(reslt);
-        var ch = Number(input.questionInt("Choose from the given options \n" +
-            "1 - buy Stock \n2 - Sell stock \n3 - print report \n  "))
-        switch (ch) {
-            case 1:
-                stockBuy.buyStock(reslt);
-                break;
-            case 2:
-                stockSell.sellStock(reslt);
-                break;
-            case 3:
-                stockPrint.printReport(reslt);
-                break;
-            case 3:
-                process.exit(0);
-            default:
-                console.log("invalid");
-        }
+    let newAcc = new path.Createacc();
+    let choice = Number(input.questionInt("Do you need to create new account \n" +
+        "1 - yes \n2 - no "))
+    if (choice == 1) {
+        newAcc.createAccount();
     }
     else {
-        console.log("credentials not found ")
+
+        let userName = input.question('Enter Username = ');
+        let password = input.question('Enter the Password = ');
+        let reslt = stockAcc.checkAccount(userName, password);
+        if (reslt != -1) {
+            console.log('Successfully Logged In');
+            stockPrint.printReport(reslt);
+            var ch = Number(input.questionInt("Choose from the given options \n" +
+                "\n1 - buy Stock \n2 - Sell stock \n3 - print report \n4 - Exit \n"))
+            switch (ch) {
+                case 1:
+                    stockBuy.buyStock(reslt);
+                    break;
+                case 2:
+                    stockSell.sellStock(reslt);
+                    break;
+                case 3:
+                    stockPrint.printReport(reslt);
+                    break;
+                case 4:
+                    process.exit(0);
+                default:
+                    console.log("invalid");
+            }
+        }
+        else {
+            console.log("credentials not found ")
+        }
     }
 }
-
 catch (err) {
     console.log(err)
 }
