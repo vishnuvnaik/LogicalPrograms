132a6efb0bc7beb2ef6770ec1816f1bdf8f62e87
= <=>
2020-03-31 15:05:06 +0530

    [Vishnu]Refactor.modified buystock function

--- stockAccount/stockAccountBL.js
+++ stockAccount/stockAccountBL.js
@@ -8,29 +8,32 @@ const fs = require('fs')
 numRestriction = /[0-9]/g;
 nameRestriction = /[A-z]/g;
 class Customer {
-    constructor(name, password, shares, amount) {
-        this.name = name;
+    constructor(userName, password, shares, amount) {
+        this.userName = userName;
         this.password = password;
         this.shares = shares;
         this.amount = amount;
     }
 }
-class Stock {
-    constructor(name, numOfStock, price) {
-        this.name = name;
-        this.numOfStock = numOfStock;
-        this.price = price;
+class Company {
+    constructor(Name, NoOfShare, Price) {
+        this.Name = Name;
+        this.NoOfShare = NoOfShare;
+        this.Price = Price;
     }
 }
 class StockAccount {
     constructor() {
         const jsonData = fs.readFileSync('stock.json');
         this.custData = JSON.parse(jsonData);
+        const comData = fs.readFileSync('company.json');
+        this.companyData = JSON.parse(comData);
+        this.compData = this.companyData.company;
     }
     checkAccount(name, password) {
         let position = -1;
         for (let i in this.custData.customer) {
-            if (this.custData.customer[i].name == name && this.custData.customer[i].password == password) {
+            if (this.custData.customer[i].userName == name && this.custData.customer[i].password == password) {
                 position = i;
             }
         }
@@ -69,26 +72,44 @@ class BuyStock extends StockAccount {
     constructor() {
         super()
     }
-    buyStock(custIndex) {
-        console.log(`-------** Welcome ${this.stockData.customer[custIndex].name}..`);
-        let name = input.question('Enter the name of Stock : ');
-        if (!nameRestriction.test(name)) { //input validation of name 
-            this.name = input.question("No special characters Invalid name! :");
+    buyStock(userName) {
+        const jsonData = fs.readFileSync('stock.json');
+        let custData = JSON.parse(jsonData);
+        const comData = fs.readFileSync('company.json');
+        this.companyData = JSON.parse(comData);
+        this.compData = this.companyData.company;
+        let compFound = 0;
+        console.log('The company list ')
+        console.log(this.compData);
+        let Name = input.question("Enter name of company to buy shares:");
+        if (!nameRestriction.test(Name)) { //input validation of name 
+            this.Name = input.question("No special characters Invalid name! :");
         }
-        let numOfStock = input.questionInt('Enter Number of stock : ');
-        if (!numRestriction.test(numOfStock)) { //input validation of numOfStock
-            this.numOfStock = input.question("Enter digits only : ");
+        let NoOfShare = input.question("Enter number of shares you want to purchase:");
+        if (!numRestriction.test(NoOfShare)) { //input validation of numOfStock
+            this.NoOfShare = input.question("Enter digits only : ");
         }
-        let price = input.questionInt('Enter the price of Stock : ');
-        if (!numRestriction.test(price)) { //input validation of numOfStock
-            this.price = input.question("Enter digits only : ");
+        let isAvai = true;
+        for (let i = 0; i < this.companyData.company.length; i++) {
+            if (this.companyData.company[i].Name == Name) {
+                isAvai = true;
+                let userShare = parseInt(Customer.shares);
+                let totalShare = NoOfShare - userShare;
+                Customer.shares = totalShare;
+                let value = parseInt(this.companyData.company[i].Price);
+                fs.writeFileSync('company.json', JSON.stringify(this.companyData));
+                let newData = fs.readFileSync('company.json', 'utf8')
+                let dataJson = JSON.parse(newData);
+                console.log(dataJson);
+                console.log('Successfully Updated');
+            }
+            else {
+                isAvai = false;
+            }
         }
-        let buyStock = new Stock(name, numOfStock, price);
-        this.stockData.customer[custIndex].stock.push(JSON.parse(JSON.stringify(buyStock)));
-        fs.writeFileSync('stock.json', JSON.stringify(this.stockData));
-        console.log('entry added ')
     }
 }
+
 class SellStock extends StockAccount {
     constructor() {
         super()
